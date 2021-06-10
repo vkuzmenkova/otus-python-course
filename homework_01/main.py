82c0ccc15da16e769e4cfcc5b666fbd858b7e319
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-06-10 21:27:55 +0300

    Update main.py
    add body to power_numbers() function
    add body to filter_numbers() function
    declare is_prime() function

--- homework_01/main.py
+++ homework_01/main.py
@@ -2,15 +2,14 @@
 Домашнее задание №1
 Функции и структуры данных
 """
-
-
-def power_numbers():
+def power_numbers(*numbers):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [num ** 2 for num in numbers]
 
 
 # filter types
@@ -18,8 +17,18 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(number):
 
-def filter_numbers():
+    if number < 2:
+        return False
+
+    # for i in range(2, math.floor(math.sqrt(number))):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+def filter_numbers(list_of_numbers: list [int], func_filter) :
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +39,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if func_filter == ODD:
+        return list(filter(lambda num: True if num % 2 != 0 else False, list_of_numbers))
+    if func_filter == EVEN:
+        return list(filter(lambda num: True if num % 2 == 0 else False, list_of_numbers))
+    if func_filter == PRIME:
+        return list(filter(is_prime, list_of_numbers))
