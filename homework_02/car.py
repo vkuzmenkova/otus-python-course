297059e45ff444d9e567cdf336ce9d3af205ba24
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-06-18 09:59:22 +0300

    add definition to set_engine()

--- homework_02/car.py
+++ homework_02/car.py
@@ -3,10 +3,11 @@
 """
 from dataclasses import dataclass
 from .base import Vehicle
+from .engine import Engine
 
 
 class Car(Vehicle):
     engine = None
 
-    def set_engine(self):
-        pass
+    def set_engine(self, engine):
+        self.engine = engine
