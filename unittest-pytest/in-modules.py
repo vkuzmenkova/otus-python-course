81be94498e199e20965f82b7108e9d80b94550b6
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-06 18:47:35 +0300

    lru_cache

--- unittest-pytest/in-modules.py
+++ unittest-pytest/in-modules.py
@@ -1,11 +1,26 @@
 import os
 from pathlib import Path
-from functools import wraps
+import functools
 import itertools
 
 cwd = os.getcwd()
 print(cwd)
 print(os.path.dirname(__file__))
 
-root = Path('/')
-print(root.parent.parent)
+
+@functools.lru_cache
+def fib(n):
+    if n < 2:
+        print(n)
+        return n
+    print(n)
+    return fib(n - 1) + fib(n - 2)
+
+
+def add(x, y):
+    return x + y
+
+
+p_add = functools.partial(add, 2)
+print(p_add(10))
+print(fib(5))
