6fab4659b066484a7f197dc24c4463640d0ade53
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-07 11:58:01 +0300

    itertools

--- unittest-pytest/in-modules.py
+++ unittest-pytest/in-modules.py
@@ -23,4 +23,16 @@ def add(x, y):
 
 p_add = functools.partial(add, 2)
 print(p_add(10))
-print(fib(5))
+# print(fib(5))
+
+# for i in itertools.chain(['A', 'B', 'C'], ["4", "5", "6"]):
+#     print(i)
+# for i in itertools.count(10, 5):
+#     if i > 100:
+#         break
+#     print(i)
+# for i in itertools.cycle('ABC'):
+#     print(i)
+# for i in itertools.accumulate([1, 2, 3], lambda x, y: x + y ** 2):
+#     print(i)
+print(list(itertools.chain.from_iterable("ABC")))
