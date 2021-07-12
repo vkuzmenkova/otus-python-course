4bba6b331497d518f18c8748c9e5b5aca5221618
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-12 11:40:14 +0300

    PUT /items/update, DELETE /items/delete

--- homework_03/main.py
+++ homework_03/main.py
@@ -20,13 +20,38 @@ def ping():
     return {"message": "pong"}
 
 
-@app.post("/items/create", summary="creates item", response_model=Item)
+@app.post("/items/create", summary="creates item", tags=["Items"],
+          response_model=Item)
 def create_item(input_item: ItemIn):
     item = Item(id=str(uuid.uuid4()), **input_item.dict())
     ITEMS_DICT[item.id] = item
     return item
 
 
-@app.get("/items/item")
-def get_item(id: str):
-    return ITEMS_DICT[id]
+@app.get("/items/get", response_model=Item, tags=["Items"],
+         summary="gets item by id")
+def get_item(input_id: str):
+    return ITEMS_DICT[input_id]
+
+
+@app.put("/items/update", summary="updates item description by id",
+         tags=["Items"],
+         response_model=Item)
+def update_item(item: Item):
+    ITEMS_DICT[item.id].name = item.name
+    ITEMS_DICT[item.id].country_of_origin = item.country_of_origin
+    ITEMS_DICT[item.id].weight = item.weight
+    ITEMS_DICT[item.id].entity = item.entity
+
+    return ITEMS_DICT[item.id]
+
+
+# key error
+@app.delete("/items/delete", summary="deletes item by id", tags=["Items"])
+def get_item(input_id: str):
+    del ITEMS_DICT[input_id]
+    return {"message": f"Item with id = {input_id} deleted."}
+
+# item = Item(name="book", country_of_origin="RU", entity=2, weight=3, id="1")
+# USER_DICT[item.id] = item
+# print(USER_DICT[item.id].name)
