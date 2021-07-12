a1be6c4997b1003bd7f06510f652563e3dbd705e
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-12 17:24:11 +0300

    fixes and comments

--- homework_03/item.py
+++ homework_03/item.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from datetime import datetime
-import uuid
 
 # пишем в csv: загружаем при каждом запуске и выгружаем при окончании
 # работы программы
@@ -14,11 +12,42 @@ class ItemBase(BaseModel):
     entity: int
     weight: float
 
+    """
+    Как проверить, что приходит именно integer? Сейчас float автоматически 
+    приводится
+    """
+
+    # @validator('entity')
+    # def is_integer(cls, value):
+    #     assert isinstance(value, int), ValueError("Value must be integer.")
+    #     return value
+
+    @validator('entity', 'weight')
+    def is_positive(cls, value):
+        assert value >= 0, ValueError("Value must be >= 0.")
+        return value
+
+
+"""
+Переопределить порядок следования полей (id выводится слишком низко)/ 
+по-другому задизайнить классы (иначе приходится повторять код и 
+переопределять валидаторы)
+
+"""
+
 
 class ItemIn(ItemBase):
     id: str
 
 
+# class ItemIn(BaseModel):
+#     id: str
+#     name: str
+#     country_of_origin: str
+#     entity: int
+#     weight: float
+
+
 class Item(ItemIn):
-    created_at: datetime
+    created_at: datetime = Field(default_factory=datetime.now)
     last_change_at: datetime
