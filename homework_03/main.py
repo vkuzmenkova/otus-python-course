5dae490b8dce3e1a0aee2944d62202c90f49db73
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-12 17:24:59 +0300

    add id_validation(), GET /items

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
-import csv
+"""
+Как правильно именовать адреса {item_id}
+"""
+
+from fastapi import FastAPI, exceptions
 import uuid
 from item import ItemBase, ItemIn, Item
 from datetime import datetime
-from typing import Optional, Dict
+from typing import Dict, List
 
 app = FastAPI()
 
-ITEMS_DICT = {}
+ITEMS_DICT: Dict[str, Item] = {}
+
+
+def id_validation(input_id: str):
+    if input_id not in ITEMS_DICT.keys():
+        raise exceptions.HTTPException(
+            404,
+            {"message": f"No item with id = {input_id}."}
+        )
 
 
 @app.get("/")
@@ -29,16 +40,18 @@ def create_item(input_item: ItemBase):
     return item
 
 
-@app.get("/items/get", response_model=Item, tags=["Items"],
+@app.get("/items/{item_id}", response_model=Item, tags=["Items"],
          summary="gets item by id")
 def get_item(input_id: str):
+    id_validation(input_id)
     return ITEMS_DICT[input_id]
 
 
 @app.put("/items/update", summary="updates item description by id",
-         tags=["Items"],
-         response_model=Item)
+         tags=["Items"], response_model=Item)
 def update_item(item: ItemIn):
+    id_validation(item.id)
+
     ITEMS_DICT[item.id].name = item.name
     ITEMS_DICT[item.id].country_of_origin = item.country_of_origin
     ITEMS_DICT[item.id].weight = item.weight
@@ -48,8 +61,17 @@ def update_item(item: ItemIn):
     return ITEMS_DICT[item.id]
 
 
-# key error
 @app.delete("/items/delete", summary="deletes item by id", tags=["Items"])
 def get_item(input_id: str):
+    id_validation(input_id)
     del ITEMS_DICT[input_id]
     return {"message": f"Item with id = {input_id} deleted."}
+
+
+@app.get("/items", summary="gets all items", tags=["Items"],
+         response_model=List[Item])
+def get_all_items():
+    tmp_list = []
+    for key in ITEMS_DICT.keys():
+        tmp_list.append(ITEMS_DICT[key])
+    return tmp_list
