7b46d14ff938e7667e8929a9c94e0ddab219f9d4
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-07-12 10:52:58 +0300

    GET /ping/, POST /items/create, GET "/item/item"

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,21 +1,32 @@
 from fastapi import FastAPI
 import csv
-from user import User, UserOut
 import uuid
+from item import ItemIn, Item
+from typing import Optional, Dict
 
 app = FastAPI()
 
+USER_DICT = {}
+ITEMS_DICT = {}
+
 
 @app.get("/")
-def root():
-    return {'hello': 'world!'}
+def root(name: str = "World"):
+    return {"Hello": name}
 
 
-@app.get("/ping/")
+@app.get("/ping/", summary="sends \"pong\" in response")
 def ping():
     return {"message": "pong"}
 
 
+@app.post("/items/create", summary="creates item", response_model=Item)
+def create_item(input_item: ItemIn):
+    item = Item(id=str(uuid.uuid4()), **input_item.dict())
+    ITEMS_DICT[item.id] = item
+    return item
 
 
-
+@app.get("/items/item")
+def get_item(id: str):
+    return ITEMS_DICT[id]
