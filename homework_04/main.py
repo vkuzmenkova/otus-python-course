bf3cbafc63dcac97cd916dfae4c9f7a1bdbfb2bb
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-10-07 11:39:00 +0300

    added async_main

--- homework_04/main.py
+++ homework_04/main.py
@@ -12,14 +12,42 @@
   (используйте полученные из запроса данные, передайте их в функцию для добавления в БД)
 - закрытие соединения с БД
 """
+import asyncio
+from typing import List
+
+from aiohttp import ClientSession
+
+from jsonplaceholder_requests import USERS_DATA_URL, POSTS_DATA_URL, fetch_json, \
+    fetch_users_data, fetch_posts_data
+
+from models import User, Post, Base, async_session, engine
 
 
 async def async_main():
-    pass
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    #
+    # users_data: List[dict]
+    # posts_data: List[dict]
+    # users_data, posts_data = await asyncio.gather(
+    #     fetch_users_data(),
+    #     fetch_posts_data(),
+    # )
+    #
+    # users: List[User]
+    # posts: List[Post]
+    #
+    # with async_session as session:
+    #     async with session.begin():
+    #         session.add_all(users)
+
+    await engine.dispose()
 
 
 def main():
-    pass
+
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
