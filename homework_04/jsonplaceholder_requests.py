bacd3011fe6cc98de1730cb05fa46cd82b9c0185
vkuzmenkova <valentinakuzmenkova@gmail.com>
2021-10-07 11:37:54 +0300

    added urls, added methods for fetching data

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,6 +1,24 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
+import asyncio
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+from aiohttp import ClientSession
+
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+async def fetch_json(session: ClientSession, url: str):
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def fetch_users_data():
+    async with ClientSession() as session:
+        return await fetch_json(session, USERS_DATA_URL)
+
+
+async def fetch_posts_data():
+    async with ClientSession() as session:
+        return await fetch_json(session, POSTS_DATA_URL)
